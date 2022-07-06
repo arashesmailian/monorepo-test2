d12202bf5608bcfaf793c21edbe846c93428830f
arashesmailian <arash.esmailian@gmail.com>
2022-07-06 15:55:00 +0430

    react-dark-mode added

--- packages/simple-react-app/src/App.tsx
+++ packages/simple-react-app/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import _ from "lodash";
 // import { QueryPayload } from "simple-express-server/server";
 import { QueryPayload } from "simple-shared-data";
+import DarkMode from "./react-dark-mode/src/DarkMode";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
         >
           GET SOME DATA
         </button>
+        <DarkMode />
       </header>
     </div>
   );
